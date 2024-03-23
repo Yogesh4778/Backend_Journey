ab16994f3e205a959a27960de5181d411098a439
Yogesh4778 <ypatidar194@gmail.com>
2024-03-23 14:25:58 +0530

    Utils setUp

---
+++ src/utils/ApiError.js
@@ -0,0 +1,28 @@
+//to handle ApiError in a standard way
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = 'Something went wrong',
+        errors = [],
+        stack = "",
+    ){
+        //overriding default
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.message = message,
+        this.success = false,
+        this.errors = errors
+
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+//wrapper function which we use in our app to keep code clean and concise
+
+
+//using promise .then .catch 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+
+
+/*
+//using try catch
+//since we have fn in argument so we can use it with the help of below syntax
+const asyncHandler = (fn) => async (req,res,next) => {
+    try{
+        await fn(req,res,next);
+    }
+    catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+}
+*/
